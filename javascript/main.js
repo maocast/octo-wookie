29c7f79e1b29fb2597b6bb29c84a51a1a064c1c9
juaneloDev <juan.castaneda2@gmail.com>
2013-12-28 21:10:40 -0800

    added base scraper functions

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,4 @@
-function doParseData(){
-	console.log("function goes here!")
-}
+
 
 $( document ).ready(function() {
   videojs('vid3', { "techOrder": ["youtube"], "src": "http://www.youtube.com/watch?v=xjS6SftYQaQ" }).ready(function() {
@@ -10,3 +8,128 @@ $( document ).ready(function() {
 		});
 	});
 });
+
+
+//SCRAPING FUNCTIONS---------------------------
+//VIDINFO CLASS
+function vidInfo(src, width, height, type){
+	this.src = src;
+	this.size = [width, height];
+	this.type = type;
+}
+
+//PARSE THE JSON DATA FROM YQL
+function doParseData(o){  
+	 alert("BEGINNING PARSE");
+	//alert("parsing data"+o.query.results.iframe[0]);
+
+	 var urlList = new Array();
+     var items = o.query.results.iframe;
+     var itemsNum=items.length;  
+	 
+     for(var i=0;i<itemsNum;i++){  
+	 
+	  	var itemInfo = new vidInfo();
+		itemInfo.src = items[i].src;
+	  	itemInfo.size = [items[i].width,items[i].height];
+		itemInfo.type = "NULL";
+		console.log(itemInfo.src + "  ***** "  + itemInfo.size[0] + itemInfo.size[1]);
+		urlList.push(itemInfo);
+	 }
+	 
+	 alert("done parsing data")
+	 
+	 //write to global variable
+	 window.myVideoList = urlList;
+	 
+	 //clean the parsed data
+	 scrapeMotionographer();
+}
+
+//CYCLE THROUGH AND PRINT THE SCRAPED DATA
+function scrapeMotionographer(){
+		
+	alert("PRINTING MOTIONOGRAPHER SCRAPE");
+	var myMoreInfo = document.getElementById("moreInfo");
+	
+	totalLinks = window.myVideoList.length;
+	
+	myMoreInfo.insertAdjacentHTML("beforeEnd", "LINKS ARE: </p>");
+	
+	for(x=0;x<totalLinks;x++){
+		//FEED THE PARSED DATA BACK INTO THE GLOBAL LIST
+		try{
+			window.myVideoList[x].src = parseSrc(window.myVideoList[x].src);
+			console.log("cleaning videoList "+window.myVideoList[x].src);
+		}
+		catch(e){
+			console.log("somthing fucked up with "+e);	
+		}
+		
+		//DETECT VIDEOTYPE
+		window.myVideoList[x].type = detectVideoType(window.myVideoList[x].src);
+		console.log("getting type "+window.myVideoList[x].type);
+
+		
+		//PRINT CUR ITEM
+		//myMoreInfo.insertAdjacentHTML("beforeEnd", src + " " + width + " " + height + "</p>" );
+		//console.log(window.myVideoList[x].src + "  ***** "  + window.myVideoList[x].size[0] + "  " + window.myVideoList[x].size[1] + "<br>");
+		
+	}
+	
+	//this call will begin a loop! (because each video instance
+	//spawns a new one upon its death.
+	
+	//playNextVideo();
+	
+}
+
+
+//PARSE SCRAPED URL STRINGS
+function parseSrc(src){
+	
+	splitSrc = src.split("?");
+	if (splitSrc.length > 0){
+		src = splitSrc[0];
+	}
+	
+	slashAt = src.indexOf("//");
+	if (slashAt == 0){
+		src = src.substring(2);
+	}
+
+	//PARSE VIMEO LINKS
+	pAt = src.indexOf("player.");
+	if(pAt == 0){
+		src = src.substring(7);
+	}
+	
+	vAt = src.indexOf("vimeo");
+	if (vAt != -1){
+		splitSrc3 = src.split("/video/");
+		if (splitSrc3.length >0){
+			src = splitSrc3[0]+"/"+splitSrc3[1];	
+		}
+	}
+	
+	//add http for those that need it
+	httpAt = src.indexOf("http://");
+	if (httpAt == -1){
+		src = ("http://"+src);
+	}
+	
+	//alert("PARSED "+src);	
+	return src;
+	
+}
+
+function detectVideoType(src){
+
+	if( src.indexOf('vimeo') >= 0){
+		return "vimeo";
+	}else if( src.indexOf('youtube') >= 0){
+		return "youtube";
+	}else{
+		return "standard";	
+	}
+}
