2ea90a4e79ccc14a5d8344a78cdf1782f73b1cca
Mao <gittest@gittest.com>
2013-12-28 23:27:32 -0500

    fixing

--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,7 @@ function doParseData(){
 
 videojs('vid3', { "techOrder": ["youtube"], "src": "http://www.youtube.com/watch?v=xjS6SftYQaQ" }).ready(function() {
 // Cue a video using ended event
-this.one('ended', function() {
-	this.src('http://www.youtube.com/watch?v=jofNR_WkoCE');
-});
+	this.one('ended', function() {
+		this.src('http://www.youtube.com/watch?v=jofNR_WkoCE');
+	});
 });
