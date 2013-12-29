645ff2dd8c08762b05d8260639a054fc80fc86fb
Mao <gittest@gittest.com>
2013-12-28 23:24:30 -0500

    file organization

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+function doParseData(){
+	console.log("function goes here!")
+}
+
+
+videojs('vid3', { "techOrder": ["youtube"], "src": "http://www.youtube.com/watch?v=xjS6SftYQaQ" }).ready(function() {
+// Cue a video using ended event
+this.one('ended', function() {
+	this.src('http://www.youtube.com/watch?v=jofNR_WkoCE');
+});
+});
